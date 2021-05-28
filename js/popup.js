8c093b2b5ecb21ac40342dc8f8f33d7279008c96
Akhil Goyal <akhilgoyal@Akhils-MacBook-Pro.local>
2021-05-28 19:18:28 -0400

    Implemented delete & state persistence.

--- js/popup.js
+++ js/popup.js
@@ -2,30 +2,64 @@ const addTaskButton = document.getElementById('add-task-btn');
 
 addTaskButton.addEventListener('click', () => addTask());
 
-const tasks = [];
+let tasks = [];
 
-const addTask = () => {
+chrome.storage.sync.get(['tasks'], (res) => {
+    tasks = res.tasks ? res.tasks : [];
+    renderTasks();
+});
 
-    const taskNum = tasks.length;
+const saveTasks = () => {
+    chrome.storage.sync.set({
+        tasks
+    });
+}
 
-    tasks.push("");
+const renderTask = (taskNum) => {
 
     const taskRow = document.createElement('div');
 
     const text = document.createElement('input');
     text.type = 'text';
     text.placeholder = 'Enter a task ...';
+    text.value = tasks[taskNum]
     text.addEventListener('change', () => {
         tasks[taskNum] = text.value;
+        saveTasks();
     });
 
     const deleteBtn = document.createElement('input');
     deleteBtn.type = 'button';
     deleteBtn.value = 'X';
+    deleteBtn.addEventListener('click', () => {
+        deleteTask(taskNum);
+    });
 
     taskRow.appendChild(text);
     taskRow.appendChild(deleteBtn);
 
     const taskContainer = document.getElementById('task-container');
     taskContainer.appendChild(taskRow);
+}
+
+const addTask = () => {
+    const taskNum = tasks.length;
+    tasks.push("");
+    renderTask(taskNum);
+    saveTasks();
+}
+
+const deleteTask = (taskNum) => {
+    tasks.splice(taskNum, 1);
+    renderTasks();
+    saveTasks();
+}
+
+const renderTasks = () => {
+    const taskContainer = document.getElementById('task-container');
+    taskContainer.textContent = "";
+
+    tasks.forEach((taskText, taskNum) => {
+        renderTask(taskNum);
+    });
 }
